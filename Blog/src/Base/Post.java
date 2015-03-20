646621d7fa5220589e4c70ff50b1549ffa9fa6f4
jdias7 <jdias@connect.ust.hk>
2015-03-20 15:35:16 +0800

    Avancée dans Post

--- Blog/src/Base/Post.java
+++ Blog/src/Base/Post.java
@@ -40,11 +40,48 @@ public class Post {
 		return "Post dqted from : " + this.date + " and which content is ; " + this.content;
 	}
 	
-	//@Override
-	/**
-	 * 
+	@Override
+	/* @param Object obj
+	 * @return boolean
 	 */
-	//public boolean equals(Object o){
-		
-	//}
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Post)) {
+			return false;
+		}
+		Post other = (Post) obj;
+		if (content == null) {
+			if (other.content != null) {
+				return false;
+			}
+		} else if (!content.equals(other.content)) {
+			return false;
+		}
+		if (date == null) {
+			if (other.date != null) {
+				return false;
+			}
+		} else if (!date.equals(other.date)) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	/* 
+	 * @return int
+	 */
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+	
 }
